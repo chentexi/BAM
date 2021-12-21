79597346c9504e1d83f8511b7228f6871fb949b2
Trent <1432984920@qq.com>
2021-12-21 16:06:23 +0800

    修改 Admin.java类

--- bam-system/src/main/java/com/trent/system/pojo/admin/Admin.java
+++ bam-system/src/main/java/com/trent/system/pojo/admin/Admin.java
@@ -2,12 +2,13 @@ package com.trent.system.pojo.admin;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.trent.common.utils.base.BaseEntity;
 import com.baomidou.mybatisplus.annotation.TableId;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
-import com.trent.common.utils.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -30,68 +31,89 @@ import org.springframework.security.core.userdetails.UserDetails;
 @TableName("admin")
 @ApiModel(value = "Admin对象", description = "")
 public class Admin extends BaseEntity implements Serializable, UserDetails{
-	
-	private static final long serialVersionUID = 1L;
-	
-	@ApiModelProperty(value = "id")
-	@TableId(value = "id", type = IdType.AUTO)
-	private Integer id;
-	
-	@ApiModelProperty(value = "姓名")
-	private String name;
-	
-	@ApiModelProperty(value = "手机号码")
-	private String phone;
-	
-	@ApiModelProperty(value = "住宅电话")
-	private String telephone;
-	
-	@ApiModelProperty(value = "联系地址")
-	private String address;
-	
-	@ApiModelProperty(value = "是否启用")
-	private Boolean enabled;
-	
-	@ApiModelProperty(value = "用户名")
-	private String userName;
-	
-	@ApiModelProperty(value = "密码")
-	private String passWord;
-	
-	@ApiModelProperty(value = "用户头像")
-	private String userFace;
-	
-	public Boolean isAdmin(){
-		return this.id != null && this.id == 1L;
-	}
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities(){
-		return null;
-	}
-	@Override
-	public String getPassword(){
-		return passWord;
-	}
-	@Override
-	public String getUsername(){
-		return userName;
-	}
-	
-	@Override
-	public boolean isAccountNonExpired(){
-		return true;
-	}
-	@Override
-	public boolean isAccountNonLocked(){
-		return true;
-	}
-	@Override
-	public boolean isCredentialsNonExpired(){
-		return true;
-	}
-	@Override
-	public boolean isEnabled(){
-		return enabled;
-	}
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "姓名")
+    private String name;
+
+    @ApiModelProperty(value = "昵称")
+    private String nickName;
+
+    @ApiModelProperty(value = "性别 1男 0女")
+    private Integer gender;
+
+    @ApiModelProperty(value = "手机号码")
+    private String phone;
+
+    @ApiModelProperty(value = "住宅电话")
+    private String telephone;
+
+    @ApiModelProperty(value = "联系地址")
+    private String address;
+
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "是否启用")
+    private Boolean enabled;
+
+    @ApiModelProperty(value = "用户名")
+    private String userName;
+
+    @ApiModelProperty(value = "密码")
+    private String passWord;
+
+    @ApiModelProperty(value = "用户头像")
+    private String userFace;
+
+    @ApiModelProperty(value = "创建人")
+    private String createBy;
+
+    @ApiModelProperty(value = "更改人")
+    private String updateBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
+    
+    public Boolean isAdmin(){
+        return this.id != null && this.id == 1L;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return null;
+    }
+    @Override
+    public String getPassword(){
+        return passWord;
+    }
+    @Override
+    public String getUsername(){
+        return userName;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return enabled;
+    }
 }
