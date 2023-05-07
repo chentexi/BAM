5b126936ba6c71659e7d1617536deea59a89de90
Trent <1432984920@qq.com>
2023-05-08 02:01:49 +0800

    flywaydb测试用例

--- bam-amdin/src/main/resources/db/migration/v1/V1__creat_table.sql
+++ bam-amdin/src/main/resources/db/migration/v1/V1__creat_table.sql
@@ -1,3 +1,3 @@
 create table if not exists fff{
     uu varchar(255) not null
-}
+} Invocation of init method failed; nested exception is org.flywaydb.core.api.FlywayException: Found non-empty schema(s) `bam` but no schema history table. Use baseline() or set baselineOnMigrate to true to initialize the schema history table.
